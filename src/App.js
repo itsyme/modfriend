09ded2f1a6bd8a377681d45675e5fb6adbd73b9c
David Lee <e0524723@u.nus.edu>
2021-05-23 14:21:39 +0800

    Changed import to relative path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-import LoginPage from "/Users/david/Desktop/Orbital/modfriend/src/LoginPage.js"
+import LoginPage from "./LoginPage.js"
 
 function App() {
   return (
