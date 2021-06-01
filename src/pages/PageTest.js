995528587158a650f9e320fd836d4407458740c3
DESKTOP-JMLAGVU\Amelia <ameliachong.23@gmail.com>
2021-06-01 12:55:52 +0800

    commit test

---
+++ src/pages/PageTest.js
@@ -0,0 +1,14 @@
+import AppShell from "../components/AppShell/AppShell";
+import LoginBar from "../components/LoginBar/LoginBar";
+
+function PageTest() {
+    return (
+        <>
+        <AppShell />
+        <p />
+        <LoginBar />
+        </>
+    )
+}
+
+export default PageTest;
