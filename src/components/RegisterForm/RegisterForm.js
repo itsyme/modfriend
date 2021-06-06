a5d0449621b5a8a3ec2b7df9f67993d82be85e95
David Lee <e0524723@u.nus.edu>
2021-06-06 11:11:18 +0800

    fixed auth signup

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -21,16 +21,16 @@ export default function RegisterForm() {
     e.preventDefault()
 
     if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError("Passwords do not match")
+      return setError("Passwords do not match");
     } 
 
-    /*try {
+    try {
       setError("")
       setLoading(true)
       await signup(emailRef.current.value, passwordRef.current.value)
     } catch {
-      setError("Failed to create an account")
-    }*/
+      setError("Email already in use")
+    } 
 
     setLoading(false)
   }
@@ -57,19 +57,19 @@ export default function RegisterForm() {
                 required id="standard-required"
                 label="Email"
                 type="email"
-                ref={emailRef} required
+                inputRef={emailRef} required
               />
               <p />
               <TextField required id="standard-required"
                 label="Password"
                 type="password"
-                ref={passwordRef} required
+                inputRef={passwordRef} required
               />
               <p />
               <TextField required id="standard-required"
                 label="Re-enter Password"
                 type="password"
-                ref={passwordConfirmRef} required
+                inputRef={passwordConfirmRef} required
               />
               <p />
               <Button variant="contained" style={{ background: "#4952ff", color: "white" }}
