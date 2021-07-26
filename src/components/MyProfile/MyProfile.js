b5afe5ceb2ce1db0a9843b9d96022dae64c59b15
David Lee <e0524723@u.nus.edu>
2021-07-26 20:29:13 +0800

    Design changes

--- src/components/MyProfile/MyProfile.js
+++ src/components/MyProfile/MyProfile.js
@@ -40,7 +40,7 @@ function MyProfile() {
         </center>
             <Paper elevation = {3} className = {styles.profileData} style = {{background: 'linear-gradient(rgba(74, 83, 107, 0.8), rgba(74, 83, 107, 0.6))',
              color: 'white',
-             borderRadius: '70px'}}>
+             borderRadius: '70px 70px 70px 20px'}}>
                 <div className = {styles.font}>
                 
                 <h3>
